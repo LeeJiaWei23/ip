c750cdf70f80c1bdef8878e71a586d8dc6179b0a
LeeJiaWei <leejiawei828@gmail.com>
2025-01-19 22:49:34 +0800

    Add greeting

---
+++ src/main/java/Clippy.java
@@ -0,0 +1,11 @@
+public class Clippy {
+    public static void main(String[] args) {
+        String horizontalLine = "____________________________________________________________\n";
+        String greeting = """
+                Hey! I'm Clippy. Your personal assistant chatbot ┌|∵|┘
+                How can I help?
+                """;
+        String goodbye = "Goodbye. Look forward to our next chat!\n";
+        System.out.println(horizontalLine + greeting + horizontalLine + goodbye + horizontalLine);
+    }
+}
