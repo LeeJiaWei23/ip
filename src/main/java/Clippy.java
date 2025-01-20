8030b410c6d9a3beddf4d3c9c6dab22676f84ef3
LeeJiaWei <leejiawei828@gmail.com>
2025-01-20 10:55:43 +0800

    Add echo functionality

--- src/main/java/Clippy.java
+++ src/main/java/Clippy.java
@@ -1,11 +1,24 @@
+import java.util.Scanner;
+
 public class Clippy {
-    public static void main(String[] args) {
+
+    static String encloseText(String input) {
         String horizontalLine = "____________________________________________________________\n";
+        return horizontalLine + input + "\n" + horizontalLine;
+    }
+
+    public static void main(String[] args) {
         String greeting = """
                 Hey! I'm Clippy. Your personal assistant chatbot ┌|∵|┘
-                How can I help?
-                """;
-        String goodbye = "Goodbye. Look forward to our next chat!\n";
-        System.out.println(horizontalLine + greeting + horizontalLine + goodbye + horizontalLine);
+                How can I help?""";
+        String goodbye = "Goodbye. Look forward to our next chat!";
+        System.out.print(encloseText(greeting));
+        Scanner reader = new Scanner(System.in);
+        String input = reader.next();
+        while (!input.equals("bye")) {
+            System.out.print(encloseText(input));
+            input = reader.next();
+        }
+        System.out.print(encloseText(goodbye));
     }
 }
