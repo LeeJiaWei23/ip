999dacb4bf72c92ceb87c3e8634841daa8915694
LeeJiaWei <leejiawei828@gmail.com>
2025-01-21 18:54:06 +0800

    Clippy.java: Use ArrayList instead of fix array

--- src/main/java/List.java
+++ src/main/java/List.java
@@ -1,56 +1,55 @@
+import java.util.ArrayList;
+
 public class List {
-    private final Task[] tasks;
-    private static final int MAX_ITEMS = 100;
-    private int currentIndex = 0;
+    private final ArrayList<Task> tasks;
 
     public List() {
-        tasks = new Task[MAX_ITEMS];
+        tasks = new ArrayList<>();
     }
 
     public void addItem(String item) {
         if (item.startsWith("todo ")) {
             String description = item.substring(5);
-            tasks[currentIndex] = new ToDo(description);
+            tasks.add(new ToDo(description));
         } else if (item.startsWith("deadline ")) {
             String text = item.substring(9);
             String[] parts = text.split("/by");
             String description = parts[0];
             String date = parts[1].trim();
-            tasks[currentIndex] = new Deadline(description, date);
+            tasks.add(new Deadline(description, date));
         } else if (item.startsWith("event ")) {
            String text = item.substring(6);
            String[] parts = text.split(" /from | /to ");
            String description = parts[0];
            String start = parts[1];
            String end = parts[2];
-           tasks[currentIndex] = new Event(description, start, end);
+           tasks.add(new Event(description, start, end));
         }
-        currentIndex++;
     }
 
     public void markTask(int index) {
-        tasks[index - 1].markAsDone();
+        tasks.get(index - 1).markAsDone();
     }
 
     public void unmarkTask(int index) {
-        tasks[index - 1].markAsUndone();
+        tasks.get(index - 1).markAsUndone();
     }
 
     public String displayList() {
         StringBuilder result = new StringBuilder("Current Tasks in Your List:\n");
-        for (int i = 0; i < currentIndex; i++) {
+        for (int i = 0; i < tasks.size(); i++) {
             result.append("     ")
                   .append(i + 1).append(".")
-                  .append(tasks[i].toString()).append("\n");
+                  .append(tasks.get(i).toString()).append("\n");
         }
         return result.toString();
     }
 
     public String getTaskDescription(int index) {
-        return tasks[index - 1].toString();
+        return tasks.get(index - 1).toString();
     }
 
     public int getTaskNum() {
-        return currentIndex;
+        return tasks.size();
     }
 }
